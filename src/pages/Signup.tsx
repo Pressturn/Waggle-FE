1e888b143429227b4d3f2b84454c20634bc0c52a
Pressturn <Prestonyeo1@gmail.com>
2025-12-05 00:44:52 +0800

    Feat: Signup simple form

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <div>
+            <h1>Signup</h1>
+            <form>
+                <div>
+                    <label>Name</label>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                    <label>Email</label>
+                    <input type="email" name="email" />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password" />
+                </div>
+                <button type="submit">Sign Up</button>
+            </form >
+        </div >
+    )
 }
 
 export default Signup
