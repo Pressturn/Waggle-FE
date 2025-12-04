f5155a194b20cf87870fab4c21368bcb9b3da375
Pressturn <Prestonyeo1@gmail.com>
2025-12-04 18:32:45 +0800

    Feat: added formData state to login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
+import { useState } from 'react'
 
 function Login() {
+
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    })
+
     return (
         <div>
             <h1>Login</h1>
             <form>
                 <div>
                     <label>Email</label>
-                    <input type="email" name="email" />
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                    />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" />
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password} 
+                        />
                 </div>
                 <button type="submit">Sign In</button>
             </form >
