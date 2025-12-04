db14ce45527fecea92aa07c361884a7a12cea714
Pressturn <Prestonyeo1@gmail.com>
2025-12-05 01:09:14 +0800

    Chore: added handlechange and formdata

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,22 +1,52 @@
-import React from 'react'
+import { useState } from 'react'
 
 function Signup() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.name
+        })
+    }
     return (
         <div>
             <h1>Signup</h1>
             <form>
                 <div>
                     <label>Name</label>
-                    <input type="text" name="name" />
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                    />
                 </div>
+
                 <div>
                     <label>Email</label>
-                    <input type="email" name="email" />
+                    <input
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
                 </div>
+
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" />
+                    <input
+                        type="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
                 </div>
+
                 <button type="submit">Sign Up</button>
             </form >
         </div >
