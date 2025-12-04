0f37f5dabaf28af77ebb560961e8462b58502d4e
Pressturn <Prestonyeo1@gmail.com>
2025-12-04 17:59:09 +0800

    Feat: Route Path for sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import MainLayout from './layouts/MainLayout'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import ActivityLog from './pages/ActivityLog'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Pets from './pages/Pets'
+import Signup from './pages/Signup'
+
 
 function App() {
-
   return (
-    <MainLayout>
-      <h1 className="text-3xl font-bold">Dashboard</h1>
-      <p className="text-gray-500 mt-2">This is where the dashboard content will go.</p>
-    </MainLayout>
+    <Routes>
+      
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      <Route path="/" element={<MainLayout><Dashboard /></MainLayout>} />
+      <Route path="/pets" element={<MainLayout><Pets /></MainLayout>} />
+      <Route path="/activity" element={<MainLayout><ActivityLog /></MainLayout>} />
+
+      <Route path="*" element={<Navigate to="/" />} />
+
+    </Routes>
   )
 }
 
