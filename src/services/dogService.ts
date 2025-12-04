b7fba93f6a0897e68884ccb932ca3f99482d93c1
Pressturn <Prestonyeo1@gmail.com>
2025-12-04 14:04:29 +0800

    Feat: dogService with Axios

--- src/services/dogService.ts
+++ src/services/dogService.ts
@@ -26,13 +26,30 @@ export interface CreateDogData {
 }
 
 const dogService = {
-
     getAll: async (): Promise<{ dogs: Dog[] }> => {
         const response = await api.get('/dogs')
         return response.data
     },
 
+    getOne: async (id: string): Promise<{ dog: Dog }> => {
+        const response = await api.get(`/dogs/${id}`)
+        return response.data
+    },
 
+    create: async (data: CreateDogData): Promise<{ message: string; dog: Dog }> => {
+        const response = await api.post('/dogs', data)
+        return response.data
+    },
+
+    update: async (id: string, data: Partial<CreateDogData>): Promise<{ message: string; dog: Dog }> => {
+        const response = await api.put(`/dog/${id}`, data)
+        return response.data
+    },
+
+    delete: async (id: string): Promise<{ message: string }> => {
+        const response = await api.delete(`/dogs/${id}`)
+        return response.data
+    }
 }
 
 export default dogService
