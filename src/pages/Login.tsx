02b314166c9d89b8ab398a65b75e8b6c5fdcfc00
Pressturn <Prestonyeo1@gmail.com>
2025-12-04 18:26:04 +0800

    Feat: simple form added for login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function Login() {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <div>
+            <h1>Login</h1>
+            <form>
+                <div>
+                    <label>Email</label>
+                    <input type="email" name="email" />
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password" />
+                </div>
+                <button type="submit">Sign In</button>
+            </form >
+        </div >
+    )
 }
 
 export default Login
