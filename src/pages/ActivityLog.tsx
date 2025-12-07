8c81413b2d8203e3432b786843a47cd65a99fe72
Pressturn <Prestonyeo1@gmail.com>
2025-12-07 16:17:34 +0800

    Feat: display all types of recent activity in log

--- src/pages/ActivityLog.tsx
+++ src/pages/ActivityLog.tsx
@@ -2,9 +2,24 @@ import { useEffect, useState } from 'react'
 import activityService from "../services/activityService"
 
 function ActivityLog() {
-  const [activities, setActivities] = useState([])
+  const [activities, setActivities] = useState<Activity[]>([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState('')
+
+  useEffect(() => {
+    fetchActivities()
+  }, [])
+
+  const fetchActivities = async () => {
+    try {
+      const data = await activityService.getAll()
+      setActivities(data.activities)
+    } catch (error) {
+      setError(error instanceof Error ? error.message : 'Failed to fetch activities')
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <div>
@@ -15,9 +30,20 @@ function ActivityLog() {
       {error && <p>Error: {error}</p>}
 
       <p> Total Activities: {activities.length}</p>
+
+      {activities.map(activity => (
+        <div key={activity.id}>
+          <p> Type: {activity.type}</p>
+          <p>Date: {activity.date}</p>
+          <p>Time: {activity.time}</p>
+          <p>Food Type:{activity.foodType}</p>
+          <p> Portion: {activity.portion}</p>
+          <p>Activity Kind: {activity.kind}</p>
+          <p>Notes: {activity.notes}</p>
+          <hr/>
+        </div>
+      ))}
     </div>
-
-
   )
 }
 
