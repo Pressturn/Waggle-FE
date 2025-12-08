a01dde9f971511957bfdee10a4fd586e774834cd
Pressturn <Prestonyeo1@gmail.com>
2025-12-08 15:05:48 +0800

    Feat: Added PetDetails route

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Pets from './pages/Pets'
 import Signup from './pages/Signup'
+import PetDetails from './pages/PetDetails'
 
 
 function App() {
   return (
     <Routes>
-      
+
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
       <Route path="/" element={<MainLayout><Dashboard /></MainLayout>} />
       <Route path="/pets" element={<MainLayout><Pets /></MainLayout>} />
+      <Route path="/pets/:dogId" element={<MainLayout><PetDetails /></MainLayout>} />
       <Route path="/activity" element={<MainLayout><ActivityLog /></MainLayout>} />
 
       <Route path="*" element={<Navigate to="/" />} />
